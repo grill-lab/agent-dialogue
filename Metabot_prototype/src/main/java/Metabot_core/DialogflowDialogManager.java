b846fdbac85d59ff40791e5d4ba2d249457a5208
Adam5i <czyzewski.i.a@gmail.com>
2018-06-07 16:43:13 +0100

    Initial commit

---
+++ Metabot_prototype/src/main/java/Metabot_core/DialogflowDialogManager.java
@@ -0,0 +1,78 @@
+package Metabot_core;
+
+
+//TODO delete the wildcard imports
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2beta1.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+
+public class DialogflowDialogManager {
+    private String _languageCode;
+    private String _sessionId;
+
+    public DialogflowDialogManager(String languageCode, String sessionId) {
+        this._languageCode = languageCode;
+        this._sessionId = sessionId;
+    }
+
+    public void set_languageCode(String _languageCode) {
+        this._languageCode = _languageCode;
+    }
+
+    public void set_sessionId(String _sessionId) {
+        this._sessionId = _sessionId;
+    }
+
+    public List<ResponseDataStructure> getResponsesFromAgentsFromText(String textPassed, Map<String, String> listOfAgentsByProjectIdAndAuthenticationKeyFile) throws IOException {
+        List<ResponseDataStructure> listOfResponses = new ArrayList<>();
+
+        // Get the response from each agent
+        for (Map.Entry<String, String> agentInformation : listOfAgentsByProjectIdAndAuthenticationKeyFile.entrySet()) {
+            String projectId = agentInformation.getKey();
+            String jsonKeyFileLocation = agentInformation.getValue();
+
+            // Authorization part
+            CredentialsProvider credentialsProvider = FixedCredentialsProvider.create((ServiceAccountCredentials
+                    .fromStream(new FileInputStream(jsonKeyFileLocation))));
+            SessionsSettings sessionsSettings = SessionsSettings.newBuilder().setCredentialsProvider(credentialsProvider).build();
+
+            // Creating SessionClient
+            SessionsClient sessionsClient = SessionsClient.create(sessionsSettings);
+            SessionName session = SessionName.of(projectId, _sessionId);
+
+            // Setting text input, sending to the server and getting response
+            TextInput.Builder textInput = TextInput.newBuilder().setText(textPassed).setLanguageCode(_languageCode);
+            QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+            DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+            QueryResult queryResult = response.getQueryResult();
+
+
+            System.out.println(response.toString());
+            //TODO save all the output as an instance of the ResponseDataStructure class
+            ResponseDataStructure responseDataStructure = new ResponseDataStructure();
+
+
+
+            /* queryResult.getAction()
+                .getQueryText()
+                .getIntent().getDisplayName()
+                .getIntentDetectionConfidence()
+                .getFulfillmentText()*/
+
+
+            listOfResponses.add(responseDataStructure);
+        }
+
+
+        return listOfResponses;
+    }
+
+
+}
---
+++ Metabot_prototype/src/main/java/Metabot_core/TestingClass.java
@@ -0,0 +1,53 @@
+package Metabot_core;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestingClass {
+    public static String _languageCode = "en-US";
+    public static String _logFileDirectory = "/Users/Adam/Documents/Internship/";
+    public static String _testedTextFileDirectory = "/Users/Adam/Documents/Internship/" +
+            "Metabot_prototype/src/main/java/Metabot_core/TestTextFiles/";
+    public static String _nameOfTestedFile = "SampleConversation.txt";
+    public static String _nameOfFileWithProjectIdAndKeysLocations = "ProjectIdAndJsonKeyFileLocations.txt";
+    public static Map<String, String> _listOfDialogflowAgentsByProjectIdAndKeyFile = new HashMap<String, String>() {
+    };
+
+
+    public static void readProjectIdAndKeyFileToHashMap(String fileLocation) throws IOException {
+        Path path = Paths.get(fileLocation);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        for (String line : lines) {
+            String[] projectIdAndJsonKey = line.split(", ");
+            _listOfDialogflowAgentsByProjectIdAndKeyFile.put(projectIdAndJsonKey[0], projectIdAndJsonKey[1]);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        DialogManager dialogManager = new DialogManager();
+        dialogManager.initialiseDialogflowDialogManagerInstanceAndLogger(_languageCode, _logFileDirectory);
+
+        // Add the Agents we want to test from text file:
+        readProjectIdAndKeyFileToHashMap(_testedTextFileDirectory + _nameOfFileWithProjectIdAndKeysLocations);
+        for (Map.Entry<String, String> agentInformation : _listOfDialogflowAgentsByProjectIdAndKeyFile.entrySet()) {
+            dialogManager.addDialogflowAgentByProjectId(agentInformation.getKey(), agentInformation.getValue());
+        }
+
+        // Call the DialogflowManager on all sentences/lines stored in a text file.
+        Path path = Paths.get(_testedTextFileDirectory + _nameOfTestedFile);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        for (String line : lines) {
+            //TODO delete print statements - however, this is testing class
+            System.out.println("TESTING CLASS OUTPUT: CURRENTLY HANDLING THE REQUEST FOR: " + line);
+            dialogManager.getResponsesFromDialogflowAgentsForTextInput(line);
+            System.out.println("TESTING CLASS OUTPUT: FINISHED HANDLING THE REQUEST FOR: " + line + "\n");
+        }
+
+    }
+}
