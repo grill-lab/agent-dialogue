d29bcdf085a084160ffcd15e7e0a982f91186cd9
Adam5i <czyzewski.i.a@gmail.com>
2018-06-11 12:15:34 +0100

    Protobuffer - in progress

--- src/main/java/edu/gla/kail/ad/core/DialogManager.java
+++ src/main/java/edu/gla/kail/ad/core/DialogManager.java
@@ -5,13 +5,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-/*  This class manages the conversation with different platforms - such as Dialogflow.
+/**
+ * This class manages the conversation with different platforms - such as Dialogflow.
  *  1) Add the list of Dialogflow Agents by project Id and Key File directory
  *  2) initialiseDialogflowDialogManager
  *  3) Do Conversations
  *
  *  _agentsByProjectIdAndKeyMap <project id, direction of the json file with authorization keys for particular agent/project>
- *  etc. */
+ **/
 //TODO(Adam) - add description of all the variables, functions, classes etc.
 
 public class DialogManager {
@@ -24,8 +25,8 @@ public class DialogManager {
 
     /* Creating an instance of DialogflowDialogManager and ConversationLogger used for the purpose of one session.
      Takes used languageCode and logFileLocation which is the directory to store log files into.*/
-    public void initialiseDialogflowDialogManagerInstanceAndLogger(String languageCode, String logFileLocation, String sessionId) throws Exception {
-        _dialogflowDialogManagerInstance = new DialogflowDialogManager(languageCode, sessionId);
+    public void initialiseDialogflowDialogManagerInstanceAndLogger(String languageCode, String logFileLocation, String sessionId, Map<String, String> agentsByProjectIdAndKeyMap) throws Exception {
+        _dialogflowDialogManagerInstance = new DialogflowDialogManager(languageCode, sessionId, agentsByProjectIdAndKeyMap);
         _conversationLogger = new ConversationLogger();
         _conversationLogger._logFileDirectory = logFileLocation;
     }
@@ -33,9 +34,9 @@ public class DialogManager {
    /*  Return the list of responses for a passed text textInput from all the agents
      stored in the _agentsByProjectIdAndKeyMap and calles storing all the responses in the log file.
      This function is also responsible for calling the logging function. */
-    public List<ResponseDataStructure> getResponsesFromDialogflowAgentsForTextInput(String textInput, Map<String, String> agentsByProjectIdAndKeyMap) throws Exception {
+    public List<ResponseDataStructure> getResponsesFromDialogflowAgentsForTextInput(String textInput) throws Exception {
         List<ResponseDataStructure> listOfResponses = _dialogflowDialogManagerInstance.
-                getResponsesFromAgentsFromText(textInput, agentsByProjectIdAndKeyMap);
+                getResponsesFromAgentsFromText(textInput);
         storeResponsesInLogs(listOfResponses);
         return listOfResponses;
     }
