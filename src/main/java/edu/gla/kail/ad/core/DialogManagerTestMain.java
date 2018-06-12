162c6ac148c82cb0a5fb87a9f966b2a1ece525e0
Adam5i <czyzewski.i.a@gmail.com>
2018-06-12 21:59:14 +0100

    Working basic version of a prototype. Needs more documentation.

--- src/main/java/edu/gla/kail/ad/core/DialogManagerTestMain.java
+++ src/main/java/edu/gla/kail/ad/core/DialogManagerTestMain.java
@@ -1,42 +1,62 @@
 package edu.gla.kail.ad.core;
 
+import com.google.cloud.Tuple;
+import com.google.protobuf.Timestamp;
+import edu.gla.kail.ad.core.Client.InputInteraction;
+import edu.gla.kail.ad.core.Client.InteractionRequest;
+import edu.gla.kail.ad.core.Client.InteractionType;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
-/* Class for testing the functionality. */
-
+/**
+ * Class for testing the functionality.
+ */
 public class DialogManagerTestMain {
+    private static List<ConfigurationTuple> _configurationTuples;
 
-    // map of agents?
-    private static Map<String, String> _mapOfSessionClientsAndSessionNames = new HashMap();
-
-    /*     Return random sessionId used to initialise the DialogflowDialogManager. */
-    private static String getRandomNumber() {
-        return UUID.randomUUID().toString();
-    }
-
-    /*   Add projectId and the directory of key files, stored in a text file with directory
-       fileDirectory to the main HashMap of all Dialogflow Agents
-       _nameOfFileWithProjectIdAndKeysLocations. */
-    public static void readProjectIdAndKeyFileToHashMap(String fileDirectory) throws IOException {
+    /**
+     * Add ConfigurationTuples to the _configurationTuples.
+     * Function prone to crashes.
+     *
+     * @param fileDirectory
+     * @throws IOException
+     */
+    private static void readProjectIdAndKeyFileToHashMap(String fileDirectory) throws Exception {
+        _configurationTuples = new ArrayList();
         Path path = Paths.get(fileDirectory);
         List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        List<Tuple<String, String>> dialogflowProjectIdAndJsonKeyFileList = new ArrayList();
         for (String line : lines) {
             String[] projectIdAndJsonKey = line.split(", ");
-            _mapOfSessionClientsAndSessionNames.put(projectIdAndJsonKey[0], projectIdAndJsonKey[1]);
+            switch (projectIdAndJsonKey[0]) {
+                case "Dialogflow":
+                    dialogflowProjectIdAndJsonKeyFileList.add(Tuple.of(projectIdAndJsonKey[1],
+                            projectIdAndJsonKey[2]));
+                    break;
+                default:
+                    throw new IllegalArgumentException("The given Dialog Manager is not supported" +
+                            " yet.");
+            }
         }
+        _configurationTuples.add(new ConfigurationTuple(SupportedDialogManagers.DIALOGFLOW,
+                dialogflowProjectIdAndJsonKeyFileList));
+    }
+
+    private static String getRandomNumberAsString() {
+        return UUID.randomUUID().toString();
     }
 
     public static void main(String[] args) throws Exception {
         String languageCode = "en-US";
+        String deviceType = "iPhone Google Assistant";
         File currentClassPathFile = new File(DialogManagerTestMain.class.getProtectionDomain()
                 .getCodeSource().getLocation().getPath()).getParentFile();
         String testTextFileDirectory = currentClassPathFile.getParent() +
@@ -47,25 +67,35 @@ public class DialogManagerTestMain {
         String nameOfTestedFile = "SampleConversation.txt";
         String nameOfFileWithProjectIdAndKeysLocations = "ProjectIdAndJsonKeyFileLocations.txt";
 
-        /* Add the Agents we want to test from text file: */
+        // Add the Agents we want to test from text file.
         readProjectIdAndKeyFileToHashMap(testTextFileDirectory +
                 nameOfFileWithProjectIdAndKeysLocations);
 
 
         DialogManager dialogManager = new DialogManager();
-        dialogManager.setUpDialogflowDialogManager(_mapOfSessionClientsAndSessionNames);
+        dialogManager.setUpDialogManagers(_configurationTuples);
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(millis / 1000)
+                .setNanos((int) ((millis % 1000) * 1000000)).build();
 
-        /* Call the DialogflowManager on all sentences/lines stored in a text file. */
+        // Call the DialogflowManager on all sentences/lines stored in a text file.
         Path path = Paths.get(testTextFileDirectory + nameOfTestedFile);
         List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
         for (String line : lines) {
             //TODO(Adam) delete print statements - however, this is testing class
             System.out.println("TESTING CLASS OUTPUT: CURRENTLY HANDLING THE REQUEST FOR: " + line);
-            dialogManager.getResponsesFromAgents(line, getRandomNumber(),
-                    languageCode);
+            InteractionRequest interactionRequest = InteractionRequest.newBuilder()
+                    .setTime(timestamp)
+                    .setClientId(getRandomNumberAsString())
+                    .setInteraction(InputInteraction.newBuilder()
+                            .setType(InteractionType.TEXT)
+                            .setText(line)
+                            .setDeviceType(deviceType)
+                            .setLanguageCode(languageCode))
+                    .build();
+            dialogManager.getResponsesFromAgents(interactionRequest);
             System.out.println("TESTING CLASS OUTPUT: FINISHED HANDLING THE REQUEST FOR: " + line
                     + "\n");
         }
-
     }
 }
