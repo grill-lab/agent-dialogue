06f556eb28502bd48814a3edd6cd69fac6910fb9
Adam5i <czyzewski.i.a@gmail.com>
2018-06-22 10:36:43 +0100

    Some tests still need implementation

---
+++ agent-dialogue-core/src/test/java/edu/gla/kail/ac/core/DialogflowAgentManagerTest.java
@@ -0,0 +1,22 @@
+package edu.gla.kail.ac.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DialogflowAgentManagerTest {
+    @Before
+    public void setUp() {
+
+    }
+    @After
+    public void cleanUp() {
+
+    }
+
+    @Test
+    public void
+}
