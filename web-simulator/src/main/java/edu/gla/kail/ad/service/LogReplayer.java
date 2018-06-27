eafa3b9ad7556efe868dedbc31088983ea6aa570
Adam5i <czyzewski.i.a@gmail.com>
2018-06-27 13:46:35 +0100

    Beginning of the work on Log Replayer

---
+++ web-simulator/src/main/java/edu/gla/kail/ad/service/LogReplayer.java
@@ -0,0 +1,14 @@
+package edu.gla.kail.ad.service;
+
+
+/**
+ * Functionality includes:
+ *  - reading the log data from LogEntry files,
+ *  - translating taken data to client requests,
+ *  - sending the requests to agent-dialogue-core through getInteractionResponse method of AgentDialogueClientService class,
+ *  - providing back ???? TODO(Adam): what should this function create?
+ */
+public class LogReplayer {
+
+    
+}
