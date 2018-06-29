fbece8727dd19838804bf9f0cfc317dc5cebade5
Adam5i <czyzewski.i.a@gmail.com>
2018-06-29 11:43:10 +0100

    Log replayer initial commit

---
+++ web-simulator/src/main/kotlin/edu/gla/kail/ad/simulator/backend/Main.kt
@@ -0,0 +1,31 @@
+//Main.kt
+import com.google.gson.Gson
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.header
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/api/ping/{count?}") {
+                var count: Int = Integer.valueOf(call.parameters["count"]?: "1")
+                if (count < 1) {
+                    count = 1
+                }
+                var obj = Array<Entry>(count, {i -> Entry("$i: Hello, World!")})
+                val gson = Gson()
+                var str = gson.toJson(obj)
+                call.response.header("Access-Control-Allow-Origin", "*")
+                call.respondText(str, ContentType.Application.Json)
+
+            }
+        }
+    }.start(wait = true)
+}
+
+data class Entry(val message: String)
