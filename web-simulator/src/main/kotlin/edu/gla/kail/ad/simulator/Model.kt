deb02482475f7e256829ae60792122d0cd3b210b
Adam5i <czyzewski.i.a@gmail.com>
2018-07-09 12:19:59 +0100

    Model can handle text requests from the user inverface

--- web-simulator/src/main/kotlin/edu/gla/kail/ad/simulator/Model.kt
+++ web-simulator/src/main/kotlin/edu/gla/kail/ad/simulator/Model.kt
@@ -12,52 +12,77 @@ import edu.gla.kail.ad.Client.InteractionType
 import edu.gla.kail.ad.Client.OutputInteraction
 import java.time.Instant
 
-
+/**
+ * Get response from agents and update conversationStateHolder when the textInput is passed from the user.
+ */
 fun getResponseFromTextInput(textInput: String) {
     var interactionRequest = getInteractionRequestFromText(textInput)
     conversationStateHolder._listOfMessages.add(Pair(textInput, interactionRequest))
-    
-    var responsePair = Pair("...", Any())
-    conversationStateHolder._listOfMessages.add(responsePair)
-    
+    val pendingPair = Pair("...", Any())
+    conversationStateHolder._listOfMessages.add(pendingPair)
     var interactionResponse = conversationStateHolder._client
             .getInteractionResponse(getInteractionRequestFromText(textInput))
-    responsePair = handleResponse(interactionResponse) // TODO(Adam): Check if the responsePair
-    // in the list is changed when the response pair is updated here!
+    handleResponse(interactionResponse)
+    conversationStateHolder._listOfMessages.remove(pendingPair) // TODO(Adam): check if this works properly.
 }
 
-fun handleResponse(interactionResponse: InteractionResponse): Pair<String, Any> {
+/**
+ * Handle response received from the ClientService.
+ * Update parameters in conversationStateHolder by calling handleOutputInteraction method.
+ */
+fun handleResponse(interactionResponse: InteractionResponse) {
     when (interactionResponse.messageStatus) {
-        ClientMessageStatus.ERROR -> Pair(interactionResponse.errorMessage, interactionResponse)
+        ClientMessageStatus.ERROR -> {
+            conversationStateHolder._listOfMessages.add(Pair(interactionResponse.errorMessage, interactionResponse))
+        }
         ClientMessageStatus.SUCCESSFUL -> {
             var outputInteractionList = interactionResponse.interactionList
             for (outputInteraction in outputInteractionList) {
-            
+                handleOutputInteraction(outputInteraction, interactionResponse)
             }
         }
-        else -> Pair("There was an error, contact the developer: " + interactionResponse.toString(),
-                interactionResponse)
+        else -> {
+            conversationStateHolder._listOfMessages.add(Pair("There was an error, contact the developer: " + interactionResponse.toString(), interactionResponse))
+        }
     }
 }
 
-fun handleOutputInteraction(outputInteraction: OutputInteraction) {
+/**
+ * Handle single outputInteraction (passed from outputInteractionList obtained from InteractionRequest).
+ */
+fun handleOutputInteraction(outputInteraction: OutputInteraction, interactionResponse: InteractionResponse) {
     when (outputInteraction.type) {
-        InteractionType.TEXT -> sda;
-        // TODO(Adam): Implement handling audio and action output.
+        InteractionType.TEXT -> {
+            conversationStateHolder._listOfMessages.add(Pair(outputInteraction.text, interactionResponse))
+        }
+    // TODO(Adam): Implement handling audio and action output.
+        InteractionType.ACTION -> {
+            throw Exception("Handling actions not available yet!")
+        }
+        InteractionType.AUDIO -> {
+            throw Exception("Handling audio not available yet!")
+        }
+        else -> {
+            throw Exception("There was an error! Not recognised OutputInteraction Type!")
+        }
     }
-    
 }
 
+/**
+ * Helper method: create InteractionRequests from text Input.
+ */
 fun getInteractionRequestFromText(textInput: String): InteractionRequest {
-    return (getInteractionRequest((getInputInteractionBuilder() as InputInteraction.Builder)
-            .setType(TEXT)
+    return (getInteractionRequestBuilder((getInputInteractionBuilder() as InputInteraction.Builder)
+            .setType(InteractionType.TEXT)
             .setText(textInput)
             .build()) as InteractionRequest.Builder)
             .build()
 }
 
-
-fun getInteractionRequest(inputInteraction: InputInteraction): InteractionRequestOrBuilder {
+/**
+ * Helper method: create InteractionRequestBuilders.
+ */
+fun getInteractionRequestBuilder(inputInteraction: InputInteraction): InteractionRequestOrBuilder {
     return InteractionRequest.newBuilder()
             .setClientId(WEB_SIMULATOR)
             .setUserID(returnUserName())
@@ -70,18 +95,27 @@ fun getInteractionRequest(inputInteraction: InputInteraction): InteractionReques
             .setInteraction(inputInteraction);
 }
 
+/**
+ * Helper method: create InputInteraction with parameters chosen in OptionsView.
+ */
 private fun getInputInteractionBuilder(): InputInteractionOrBuilder {
     return InputInteraction.newBuilder()
             .setLanguageCode(conversationStateHolder._language.getValue())
             .setDeviceType(deviceType())
 }
 
+/**
+ * Return the current user name.
+ * TODO(Adam): Implement getting username from database or having a username object!
+ */
 fun returnUserName(): String {
-    // TODO(Adam): Implement getting username.
     return "sampleUserName-to-be-implemented"
 }
 
+/**
+ * Return the device type.
+ * TODO(Adam): Implement getting device type.
+ */
 fun deviceType(): String {
-    // TODO(Adam): Implement getting device type.
     return "sampleDeviceType-to-be-implemented"
 }
