6ea8e42f178e5ca2a1f87bc3b1ef1055fe3b00da
Adam5i <czyzewski.i.a@gmail.com>
2018-07-11 18:04:34 +0100

    Form included

--- web-simulator/web-simulator-react/src/App.js
+++ web-simulator/web-simulator-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Agent_logo from './resources/Agent_logo.jpg'
 import './resources/App.css';
 
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={Agent_logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Agent Dialogue Web Simulator</h1>
         </header>
       </div>
     );
---
+++ web-simulator/web-simulator-react/src/InputField.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './resources/InputField.css';
+
+class InputField extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        // TODO(Adam): implement sending request!
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className = "input-box" onSubmit = {this.handleSubmit}>
+                <label>
+                    <div>Chat with Agents:</div>
+                    <input className = "input-field" type = "text" value = {this.state.value}
+                           onChange = {this.handleChange}/>
+                </label>
+                <input className = "submit-button" type = "submit" value = "Submit"/>
+            </form>
+        );
+    }
+}
+
+export default InputField;
