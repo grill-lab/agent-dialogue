75ac638e8aeb076367a84ab50ad509d73d8d7922
Adam5i <czyzewski.i.a@gmail.com>
2018-07-11 18:04:34 +0100

    Form included

--- web-simulator/web-simulator-react/src/index.js
+++ web-simulator/web-simulator-react/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './resources/App.css';
 import App from './App';
+import InputField from './InputField'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<InputField/>, document.getElementById('inputField'));
 registerServiceWorker();
