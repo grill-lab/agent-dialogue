ea70ce567aa550b01e70a27cbc01a1d9cd0f503c
Adam5i <czyzewski.i.a@gmail.com>
2018-07-15 19:10:21 +0100

    jQuery for adding languages

---
+++ simulator/src/main/webapp/js/onLoad.js
@@ -0,0 +1,34 @@
+/**
+ * Do on loading HTML.
+ */
+$(document).ready(function () {
+    populateLanguages();
+});
+
+
+// Populate languages from LANGUAGES dictionary.
+let LANGUAGES = {
+    AMERICAN_ENGLISH: {value: "en-US", name: "American English"},
+    BRITISH_ENGLISH: {value: "en-UK", name: "British English"},
+};
+
+function populateLanguages() {
+    var $languageFieldset = $('<form id = "language-fieldset">');
+
+    for (var language in LANGUAGES) {
+        var parameters = LANGUAGES[language];
+        var $label = $("<label for=" + parameters.value + ">").text(parameters.name);
+        var $input = $('<input type="radio">').attr({
+            id: parameters.value,
+            name: parameters.name,
+            value: parameters.value
+        });
+
+        $languageFieldset.append($label)
+            .append($input)
+            .append($('<br>'));
+
+        $('.options').append($languageFieldset);
+    }
+}
+
