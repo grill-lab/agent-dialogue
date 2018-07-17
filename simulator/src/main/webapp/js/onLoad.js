85926d40f0fcc6812d36d47dde3d63f519309108
Adam5i <czyzewski.i.a@gmail.com>
2018-07-17 12:42:47 +0100

    Rating correction

--- simulator/src/main/webapp/js/onLoad.js
+++ simulator/src/main/webapp/js/onLoad.js
@@ -4,8 +4,15 @@
 $(document).ready(function () {
     populateLanguages();
     $('#awaiting-responses').text(0);
-});
 
+    // When user presses "enter" in textarea, the request is being sent.
+    $('#message').keypress(function(keyPressed){
+        if(keyPressed.which == 13 && !keyPressed.shiftKey){
+            keyPressed.preventDefault();
+            sendRequestToServlet();
+        }
+    });
+});
 
 // Populate languages from LANGUAGES dictionary.
 let LANGUAGES = {
