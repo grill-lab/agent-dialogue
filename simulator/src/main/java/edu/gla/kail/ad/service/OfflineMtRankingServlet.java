806032a1d22d02a94ac48d5f6d3c2d25d1e03204
Adam5i <czyzewski.i.a@gmail.com>
2018-07-25 15:33:58 +0100

    Working through backend

--- simulator/src/main/java/edu/gla/kail/ad/service/OfflineMtRankingServlet.java
+++ simulator/src/main/java/edu/gla/kail/ad/service/OfflineMtRankingServlet.java
@@ -5,13 +5,21 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.gson.JsonObject;
+import com.google.protobuf.Timestamp;
+import com.sun.tools.internal.xjc.Language;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @WebServlet("/offline-mt-ranking-servlet")
@@ -26,7 +34,8 @@ public class OfflineMtRankingServlet extends HttpServlet {
                 response.getWriter().write(verifyUser(userId).toString());
                 break;
             case "loadTasks":
-                Integer maxTasksAssigned = Integer.valueOf(request.getParameter("maxTasksAssigned"));
+                Integer maxTasksAssigned = Integer.valueOf(request.getParameter
+                        ("maxTasksAssigned"));
                 response.getWriter().write(loadTasks(userId, maxTasksAssigned));
                 break;
             case "rateTask":
@@ -45,12 +54,63 @@ public class OfflineMtRankingServlet extends HttpServlet {
 
     private String loadTasks(String userId, Integer maxTasksAssigned) {
         JsonObject json = new JsonObject();
+
         //     check if the user has any tasks assigned,
         //         not maxNum..:
         //             assign more tasks
         //     download assigned tasks
+        DocumentReference userDocRef = _database.collection("clientWebSimulator").document
+                ("agent-dialogue-experiments").collection("users").document(userId);
+        ApiFuture<DocumentSnapshot> userFuture = userDocRef.get();
+        try {
+            DocumentSnapshot user = userFuture.get();
+            if (user.exists()) {
+                Map<String, Object> userData = user.getData();
+                ArrayList<String> listOfOpenTaskIds = (ArrayList<String>) userData.get
+                        ("openTaskIds");
+                int numOpenReferences = 0;
+                if (listOfOpenTaskIds != null) {
+                    numOpenReferences = listOfOpenTaskIds.size();
+                }
+                ArrayList<String> listOfCompletedTaskIds = (ArrayList<String>) userData.get
+                        ("completedTaskIds");
+
+
+                for (int i = maxTasksAssigned; i > numOpenReferences; i--) {
+                    // check if we can create any more ratings from database - is there anything
+                    // left?
+                    // if not, then break
+                    // create open rating for this user
+                    // add reference to particular open rating for this user
+                    // add reference to a task for this rating
+                }
+
+                // Get updated user document.
+                user = userFuture.get();
+                userData = user.getData();
+                listOfOpenTaskIds = (ArrayList<String>) userData.get("openTaskIds");
+
+                ArrayList<Object> listOfTasks = new ArrayList<>();
+                for (String taskId : listOfOpenTaskIds) {
+                    Map<String, Object> taskMap = _database.collection("clientWebSimulator")
+                            .document("agent-dialogue-experiments").collection("tasks").document
+                                    (taskId).get().get().getData();
+                    JsonObject taskJson = new JsonObject();
+                    taskJson.addProperty("clientId", (String) taskMap.get("clientId"));
+                    taskJson.addProperty("deviceType", (String) taskMap.get("deviceType"));
+                    taskJson.addProperty("language_code", (String) taskMap.get("language_code"));
+                    taskJson.addProperty("turns", ((ArrayList<Object>) taskMap.get("turns")).toString());
+                    listOfTasks.add(taskJson.toString());
+                }
+                json.addProperty("tasks", listOfTasks.toString());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         return json.toString();
     }
+
+
     private String rateTask(String userId, Integer ratingScore, String taskId) {
         JsonObject json = new JsonObject();
         json.addProperty("sth", "value");
@@ -60,7 +120,6 @@ public class OfflineMtRankingServlet extends HttpServlet {
         // update rating reference list of a task in Firestore
         return json.toString();
     }
-
 
 
     private Boolean verifyUser(String userId) {
@@ -78,6 +137,14 @@ public class OfflineMtRankingServlet extends HttpServlet {
             e.printStackTrace();
         }
         return false;
+    }
 
+    private Timestamp getTimeStamp() {
+        return Timestamp.newBuilder()
+                .setSeconds(Instant.now()
+                        .getEpochSecond())
+                .setNanos(Instant.now()
+                        .getNano())
+                .build();
     }
 }
