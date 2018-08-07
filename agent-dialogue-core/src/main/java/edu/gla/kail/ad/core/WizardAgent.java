5e758638096f842398c54fb0585fd1b44ecf4ca8
Jeff Dalton <jeff.dalton.gla@gmail.com>
2018-08-07 15:11:29 +0100

    update the wizard to use the config

--- agent-dialogue-core/src/main/java/edu/gla/kail/ad/core/WizardAgent.java
+++ agent-dialogue-core/src/main/java/edu/gla/kail/ad/core/WizardAgent.java
@@ -1,36 +1,32 @@
 package edu.gla.kail.ad.core;
 
 import com.google.api.core.SettableApiFuture;
-import com.google.cloud.firestore.*;
-import edu.gla.kail.ad.Client;
-import edu.gla.kail.ad.Client.InteractionRequest;
-import edu.gla.kail.ad.Client.InteractionType;
-import edu.gla.kail.ad.Client.OutputInteraction;
-import edu.gla.kail.ad.core.Log.ResponseLog;
-import edu.gla.kail.ad.CoreConfiguration.Agent;
-import edu.gla.kail.ad.CoreConfiguration.ServiceProvider;
-import edu.gla.kail.ad.core.Log.Slot;
-import edu.gla.kail.ad.core.Log.SystemAct;
-
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.*;
+import com.google.cloud.firestore.EventListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.protobuf.Timestamp;
+import com.google.protobuf.Value;
+import edu.gla.kail.ad.Client;
+import edu.gla.kail.ad.Client.InteractionRequest;
+import edu.gla.kail.ad.Client.InteractionType;
+import edu.gla.kail.ad.Client.OutputInteraction;
+import edu.gla.kail.ad.CoreConfiguration.AgentConfig;
+import edu.gla.kail.ad.CoreConfiguration.ServiceProvider;
+import edu.gla.kail.ad.core.Log.ResponseLog;
+import edu.gla.kail.ad.core.Log.SystemAct;
 
 import javax.annotation.Nullable;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -41,21 +37,19 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 class WizardAgent implements AgentInterface {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(WizardAgent.class);
+
+  // The firestore database connection.
   private Firestore _database;
 
-  private String _firebaseCredentialsPath =
-          "src/main/resources/agentdialogue-2cd4b-firebase-adminsdk-z39zw" +
-          "-4d5427d1fc.json";
-
   // Analogous to the "wizard project" - something like a project id in a dialogflow agent.
-  private String _projectId = "Ct5UbiTWQmkDbmF0aJrt";
+  private String _projectId;
 
-  // A unique ID passed set in the constructor, passed by DialogAgentManager.
-  private String _sessionId = "1";
+  // Default timeout to wait for a response in seconds.
+  private static final long DEFAULT_TIMEOUT_SECONDS = 60;
 
-  private String _conversationId = "NslkRXnAntZJa2QRvet6";
-
-  private static final long TIMEOUT_SECONDS = 180;
+  // Hold the wizard agent's configuration, including necessary db credentials.
+  private AgentConfig _agent;
 
   /**
    * Construct a new WizardAgent.
@@ -63,8 +57,9 @@ class WizardAgent implements AgentInterface {
    * @param sessionId
    * @throws Exception
    */
-  public WizardAgent(String sessionId) throws Exception {
-    _sessionId = sessionId;
+  public WizardAgent(String sessionId, AgentConfig agent) throws Exception {
+    _agent = agent;
+    _projectId = _agent.getProjectId();
     initAgent();
   }
 
@@ -74,7 +69,7 @@ class WizardAgent implements AgentInterface {
    * @throws Exception
    */
   private void initAgent() throws Exception {
-    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(_firebaseCredentialsPath));
+    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(_agent.getConfigurationFileURL()));
     checkNotNull(credentials, "Credentials used to initialise FireStore are null.");
 
     FirebaseOptions options = new FirebaseOptions.Builder()
@@ -84,79 +79,148 @@ class WizardAgent implements AgentInterface {
     _database = FirestoreClient.getFirestore();
   }
 
-  private CollectionReference getDbCollection() {
+  /**
+   * Specify the firestore configuration where messages will be read / written.
+   * <p>
+   * TODO(Jeff): Make the db location configurable from an agent config file.
+   *
+   * @param conversationId
+   * @return
+   */
+  private CollectionReference getDbCollection(String conversationId) {
     CollectionReference collectionReference = _database.collection("wizard")
             .document(_projectId)
             .collection("conversations")
-            .document(_conversationId)
+            .document(conversationId)
             .collection("messages");
     return collectionReference;
   }
 
   @Override
   public ServiceProvider getServiceProvider() {
-    return ServiceProvider.WIZARD;
+    return _agent.getServiceProvider();
   }
 
   @Override
-  public ResponseLog getResponseFromAgent(InteractionRequest interactionRequest) throws InterruptedException, ExecutionException, TimeoutException {
-    String responseId = "made up response id";
-      if (userExit(interactionRequest)) {
-        Map<String, Object> data = new HashMap<>();
-        data.put("interaction_text", "Goodbye!");
-        return buildResponse(responseId, data);
-      }
-      return addUserRequestWaitForReply(interactionRequest, isRequestFromUser(interactionRequest));
+  public ResponseLog getResponseFromAgent(InteractionRequest interactionRequest) throws Exception {
+    String responseId = ResponseIdGenerator.generate();
+    if (userExit(interactionRequest)) {
+      Map<String, Object> data = new HashMap<>();
+      data.put("interaction_text", "Goodbye!");
+      return buildResponse(responseId, data);
+    }
+    return addUserRequestWaitForReply(responseId, interactionRequest);
   }
 
+
+  /**
+   * Determine whether a request is from a wizard or not. This should be contained
+   * in the request parameters.
+   * TODO(Jeff): Fix how a wizard is detected
+   *
+   * @param interactionRequest
+   * @return true if the request is from the user (and not a wizard)
+   */
   private boolean isRequestFromUser(InteractionRequest interactionRequest) {
-    return interactionRequest.getUserId().startsWith("ADwizard");
+    return !interactionRequest.getUserId().startsWith("ADwizard");
   }
 
+  /**
+   * Simple intent detector to exit the the conversation.
+   *
+   * @param interactionRequest
+   * @return true if the intent is to exit the conversation
+   */
   private boolean userExit(InteractionRequest interactionRequest) {
     return interactionRequest.getInteraction().getText().toLowerCase().equals("exit");
   }
 
-  private ResponseLog addUserRequestWaitForReply(InteractionRequest interactionRequest, boolean isFromUser) throws InterruptedException, ExecutionException, TimeoutException {
-    final SettableApiFuture<ResponseLog> future = SettableApiFuture.create();
-    System.out.println("Handling user request.");
+  /**
+   * The core of the wizard. This adds the current message to the database and waits for a reply.
+   *
+   * It listens on the conversation messages. When a new message is added it returns a response
+   * with the given input text.
+   *
+   * @param responseId
+   * @param interactionRequest
+   * @return
+   * @throws InterruptedException
+   * @throws ExecutionException
+   * @throws TimeoutException
+   */
+  private ResponseLog addUserRequestWaitForReply(String responseId,
+                                                 InteractionRequest interactionRequest)
+          throws InterruptedException, ExecutionException, TimeoutException {
     checkNotNull(interactionRequest, "The passed interaction request is null!");
-    String responseId = "made up response id";
-    DocumentReference documentReference = addInteractionRequestToDatabase(_conversationId, interactionRequest, isFromUser);
+
+    LOGGER.debug("Handling request from client" + interactionRequest.getClientId());
+
+    // Create a future for our aysnc response.
+    final SettableApiFuture<ResponseLog> future = SettableApiFuture.create();
+
+    // Get the conversation id from the request parameters.
+    Map<String, Value> fieldsMap = interactionRequest.getAgentRequestParameters().getFieldsMap();
+    if (!fieldsMap.containsKey("conversation_id")) {
+      throw new IllegalArgumentException("Request must specify the conversation_id in the agent request parameters.");
+    }
+    String conversationId = fieldsMap.get("conversation_id").getStringValue();
+
+    DocumentReference documentReference =
+            addInteractionRequestToDatabase(responseId, conversationId, interactionRequest);
+
     // Now we wait for a response!
-    CollectionReference conversationCollection = getDbCollection();
+    CollectionReference conversationCollection = getDbCollection(conversationId);
+
+    // Wait for a response after the current time (filter out past messages).
     Query query = conversationCollection
             .whereGreaterThan("timestamp", com.google.cloud.Timestamp.now());
-           // .whereEqualTo("from_user", !isFromUser);
-    System.out.println("Waiting on listener: " + query.toString());
+    LOGGER.debug("Waiting on listener: " + query.toString());
+    // TODO(Jeff): Replace with lambda.
     ListenerRegistration registration = query.addSnapshotListener(new EventListener<QuerySnapshot>() {
       @Override
       public void onEvent(@Nullable QuerySnapshot snapshots, @Nullable FirestoreException e) {
         if (e != null) {
-          System.err.println("Listen failed: " + e);
+          LOGGER.error("Listen failed: " + e);
           return;
         }
         ResponseLog response = null;
         if (snapshots != null && !snapshots.isEmpty()) {
           List<DocumentChange> documentChanges = snapshots.getDocumentChanges();
-          System.out.println("num document change:" + documentChanges.size());
+          LOGGER.debug("Num document changes:" + documentChanges.size());
           DocumentChange lastChange = documentChanges.get(documentChanges.size() - 1);
+          Map<String, Object> changeData = lastChange.getDocument().getData();
+
+          if (changeData.get("user_id").equals(interactionRequest.getUserId())) {
+            // Message is from the same user, don't build a response from the same user.
+            LOGGER.info("Ignoring change from the same user:" + interactionRequest.getUserId());
+            return;
+          }
           switch (lastChange.getType()) {
             case ADDED:
-              response = buildResponse(responseId, lastChange.getDocument().getData());
+              response = buildResponse(responseId, changeData);
           }
+          // Why do we need to do this?
           if (!future.isDone()) {
             future.set(response);
           }
         }
       }
     });
-
+    final ResponseLog response = future.get(DEFAULT_TIMEOUT_SECONDS, TimeUnit.SECONDS);
     // Stop listening to changes
-    //  registration.remove();
-    return future.get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+    registration.remove();
+    return response;
   }
 
+  /**
+   * Builds the response message from a firestore database message.
+   *
+   * Currently, this only sends back the interaction_text from the data.
+   *
+   * @param responseId
+   * @param data
+   * @return
+   */
   private ResponseLog buildResponse(String responseId, Map<String, Object> data) {
     Timestamp timestamp = Timestamp.newBuilder()
             .setSeconds(Instant.now()
@@ -165,30 +229,38 @@ class WizardAgent implements AgentInterface {
                     .getNano())
             .build();
 
-    String responseString = data.getOrDefault("interaction_text",
-            "I'm sorry, there's an error.").toString();
-
-    ResponseLog.Builder responseLogBuilder = ResponseLog.newBuilder()
-            .setResponseId(responseId)
-            .setTime(timestamp)
-            .setServiceProvider(ServiceProvider.DIALOGFLOW)
-            .setRawResponse("Wizard responds with: " + responseString);
+    // Get the input text, use a fallback
+    Object messageText = data.get("interaction_text");
+    String responseString = null;
+    if (messageText != null) {
+      responseString = (String) messageText;
+    } else {
+      LOGGER.error("Message does not contain text. Returning fallback. For response: " + responseId);
+      responseString = "I'm sorry, message does not contain text.";
+    }
 
     return ResponseLog.newBuilder()
             .setResponseId(responseId)
             .setTime(timestamp)
             .setClientId(Client.ClientId.EXTERNAL_APPLICATION)
             .setServiceProvider(ServiceProvider.WIZARD)
-            .setRawResponse(responseString)
+            .setRawResponse("Text response: " + responseString)
             .addAction(SystemAct.newBuilder().setInteraction(OutputInteraction.newBuilder()
-                    .setType(InteractionType.TEXT) // TODO(Jeff): If more advanced response for wizards.
+                    .setType(InteractionType.TEXT) // TODO(Jeff): Support more advanced actions.
                     .setText(responseString)
                     .build())
             ).build();
   }
 
-  private DocumentReference addInteractionRequestToDatabase(String responseId, InteractionRequest interactionRequest, boolean isFromUser) {
-    DocumentReference chatReference = getDbCollection().document(responseId);
+  /**
+   * Add an interaction request to the message database.
+   * @param responseId
+   * @param conversationId
+   * @param interactionRequest
+   * @return Reference to the document added.
+   */
+  private DocumentReference addInteractionRequestToDatabase(String responseId, String conversationId, InteractionRequest interactionRequest) {
+    DocumentReference chatReference = getDbCollection(conversationId).document(responseId);
     Map<String, Object> data = new HashMap();
     data.put("response_id", responseId);
     data.put("client_id", interactionRequest.getClientIdValue());
@@ -206,7 +278,6 @@ class WizardAgent implements AgentInterface {
     data.put("interaction_action_list", interactionRequest.getInteraction().getActionList
             ().toString());
     data.put("timestamp", com.google.cloud.Timestamp.now());
-    data.put("from_user", isFromUser);
     chatReference.set(data);
     return chatReference;
   }
