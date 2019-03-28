5019524ce16419b3f9f703ce91571d951be531f1
Anton Leuski <leuski@ict.usc.edu>
2019-03-28 13:25:22 -0700

    Added gRPC-web stuff. Got the files to compile.

--- agent-dialogue-ui/src/App.tsx
+++ agent-dialogue-ui/src/App.tsx
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import logo from "./resources/img/Agent_logo.jpg"
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Agent Dialogue Web Simulator</h1>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
