bebfccef5802d8a48d86ed6f972d64fb5972a266
Anton Leuski <leuski@ict.usc.edu>
2019-04-03 16:29:57 -0700

    initial implementation of the connection class

--- agent-dialogue-ui/src/components/MessageModel.ts
+++ agent-dialogue-ui/src/components/MessageModel.ts
@@ -3,18 +3,21 @@ import {PartialBy} from "../common/util"
 export interface IMessage {
   text: string
   time: Date
-  speaker: string
+  speaker?: string
 }
 
-type IMessageArgument = PartialBy<IMessage, "time">
+export type IMessageArgument = PartialBy<IMessage, "time">
 
 export class Message implements IMessage {
   constructor(model: IMessageArgument) {
     Object.assign(this, {...model, time: model.time || new Date()})
   }
 
+  // noinspection JSUnusedGlobalSymbols
   public readonly text!: string
+  // noinspection JSUnusedGlobalSymbols
   public readonly time!: Date
-  public readonly speaker!: string
+  // noinspection JSUnusedGlobalSymbols
+  public readonly speaker?: string
 }
 
--- agent-dialogue-ui/src/readme_proto.txt
+++ agent-dialogue-ui/src/readme_proto.txt
@@ -1,6 +1,9 @@
 How to generate protobuf files:
 
-
+protoc -I../../agent-dialogue-core/src/main/proto/ \
+	--js_out=import_style=commonjs:generated \
+	--grpc-web_out=import_style=typescript,mode=grpcwebtext:generated \
+	service.proto client.proto
 
 If the compiler complains, you need to modify the generated .js files by hand:
 
@@ -17,4 +20,5 @@ If you see "Cannot compile namespaces when the '--isolatedModules' flag is provi
 after
   // GENERATED CODE -- DO NOT EDIT!
 add
-  export const _ = '';
+  // @ts-ignore
+
