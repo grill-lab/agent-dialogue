51df3436263fe1cb8be2f3ef833b7b8c1307211d
Anton Leuski <leuski@ict.usc.edu>
2019-04-11 12:10:04 -0700

    trying to get WoZ chat to work

--- agent-dialogue-ui/src/woz/WoZPanel.tsx
+++ agent-dialogue-ui/src/woz/WoZPanel.tsx
@@ -1,78 +1,174 @@
 import React from "react"
-import {Form, Grid} from "semantic-ui-react"
+import {Button, Form, InputOnChangeData, Segment} from "semantic-ui-react"
+import {StringMap} from "../App"
+import {ADConnection} from "../common/ADConnection"
 import {ChatComponent} from "../components/ChatTranscript"
-import {
-  Dialogue,
-  sampleDialogue,
-  US,
-} from "../components/DialogueModel"
-import css from "./WoZPanel.module.css"
+import {Dialogue} from "../components/DialogueModel"
 
 interface IWozPanelState {
-  dialogue: Dialogue
+  connection?: ADConnection
+  params: IWozParams
 }
 
 interface IWozPanelProperties {
+  params: StringMap
   dialogue?: Dialogue
 }
 
-export class WoZPanel extends React.Component<IWozPanelProperties, IWozPanelState> {
+interface IWozParams {
+  url: string
+  userID: string
+  conversationID: string
+}
+
+interface WoZParamFormProperties {
+  params: IWozParams
+  onSubmit: (params: IWozParams) => void
+}
+
+const areParamsValid = (params: IWozParams) => {
+  return params.url !== ""
+         && params.conversationID !== ""
+         && params.userID !== ""
+}
+
+export class WoZPanel
+    extends React.Component<IWozPanelProperties, IWozPanelState> {
+
+  private onSubmit = (params: IWozParams) => {
+    if (areParamsValid(params)) {
+      this.setState({
+        connection: new ADConnection(params.url),
+        params,
+      })
+    }
+  }
+
+  public render(): React.ReactNode {
+    return this.state.connection === undefined
+           ? <WoZParamForm
+               params={this.state.params}
+               onSubmit={this.onSubmit}
+           />
+           : <WoZDialogue
+               connection={this.state.connection}
+               dialogue={this.props.dialogue}
+               params={this.state.params}
+           />
+  }
 
   constructor(props: IWozPanelProperties) {
     super(props)
 
+    console.log(this.props)
+
+    const params: IWozParams = {
+      conversationID: (props.params.conversationID || "").trim(),
+      userID: (props.params.userID || "").trim(),
+      url: (props.params.url || "").trim(),
+    }
+
     this.state = {
-      dialogue: props.dialogue === undefined
-                ? sampleDialogue() : props.dialogue,
+      connection: areParamsValid(params)
+                  ? new ADConnection(params.url) : undefined,
+      params,
+    }
+  }
+}
+
+class WoZParamForm extends React.Component<WoZParamFormProperties, IWozParams> {
+
+  handleChange = (_e: any, data: InputOnChangeData) => {
+    this.setState((prev) => (
+        {...prev, [data.name]: data.value.trim()}
+    ))
+  }
+
+  handleSubmit = () => {
+    if (areParamsValid(this.state)) {
+      this.props.onSubmit(this.state)
     }
   }
 
+  public render(): React.ReactNode {
+    const {conversationID, url, userID} = this.state
+
+    return <Segment>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Input label="Host URL" name="url" value={url}
+                    onChange={this.handleChange}/>
+        <Form.Input label="User ID" name="userID" value={userID}
+                    onChange={this.handleChange}/>
+        <Form.Input label="Conversation ID" name="conversationID"
+                    value={conversationID} onChange={this.handleChange}/>
+        <Button type='submit'>Submit</Button>
+
+      </Form>
+    </Segment>
+  }
+
+  constructor(props: WoZParamFormProperties) {
+    super(props)
+    this.state = props.params
+  }
+}
+
+interface IWoZDialogueProperties {
+  dialogue?: Dialogue
+  connection: ADConnection
+  params: IWozParams
+}
+
+interface IWoZDialogueState {
+  dialogue: Dialogue
+}
+
+class WoZDialogue
+    extends React.Component<IWoZDialogueProperties, IWoZDialogueState> {
+
   private onEnter = (text: string) => {
+    this.props.connection.send({
+      conversationID: this.props.params.conversationID,
+      userID: this.props.params.userID,
+      text
+    })
     this.setState((prev) => {
       const d = prev.dialogue
-      d.append({speaker: US, text})
+      d.append({speaker: this.props.params.userID, text})
       return {dialogue: d}
     })
   }
 
   public render(): React.ReactNode {
+    return <ChatComponent
+        dialogue={this.state.dialogue}
+        us={this.props.params.userID}
+        onEnter={this.onEnter}
+    />
+  }
 
-    const userType: string = "user"
+  constructor(props: IWoZDialogueProperties) {
+    super(props)
 
-    const handleChange = (): void => {}
+    console.log(this.props)
 
-    return <Grid className={css.mainGrid}>
-    <Grid.Row>
-      <Grid.Column width={13}>
-        <ChatComponent
-            dialogue={this.state.dialogue}
-            us={US}
-            onEnter={this.onEnter}
-        />
-      </Grid.Column>
+    this.state = {
+      dialogue: props.dialogue === undefined
+                ? new Dialogue({messages: []}) : props.dialogue,
+    }
 
-      <Grid.Column width={3}>
-        <Form>
-          <Form.Input fluid label="User ID"/>
-          <Form.Group inline>
-            <label>Type</label>
-            <Form.Radio
-                label="Wizard"
-                value="wizard"
-                checked={userType === "wizard"}
-                onChange={handleChange}
-            />
-            <Form.Radio
-                label="User"
-                value="user"
-                checked={userType === "user"}
-                onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Input fluid label="Conversation ID"/>
-        </Form>
-      </Grid.Column>
-    </Grid.Row>
-    </Grid>
+    this.props.connection.subscribe({
+      onResponse: (response => {
+        console.log(response.getUserId())
+        console.log(response.getInteractionList()[0].getText())
+        // this.setState((prev) => {
+        //   const d = prev.dialogue
+        //   d.append({speaker: this.props.params.userID, text: response.get})
+        //   return {dialogue: d}
+        // })
+      }),
+      conversationID: this.props.params.conversationID,
+      userID: this.props.params.userID,
+    })
   }
 }
